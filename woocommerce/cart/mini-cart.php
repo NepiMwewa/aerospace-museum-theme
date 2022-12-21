9df813eff4f1d39155cf3c54e428e926f69b7ad9
Nepimwewa <wolfaceking@gmail.com>
2022-12-20 23:20:11 -0800

    Fixed mini cart issues

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -88,6 +88,9 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 
 <?php else : ?>
 
+	<a class="cart-close" href="javascript:void(0);" onclick="toggleCart()">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+  </a>
 	<p class="woocommerce-mini-cart__empty-message"><?php esc_html_e( 'No products in the cart.', 'woocommerce' ); ?></p>
 
 <?php endif; ?>
